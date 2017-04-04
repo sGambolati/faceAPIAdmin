24547ee212cce048ead16a346b6a9912bb3b8b55
Sebastian Ariel Gambolati <sebastian.gambolati@southworks.com>
2017-04-04 11:20:15 -0300

    Add UI to show recognized faces in Try Controller.

--- FaceRecMVC/Controllers/TryController.cs
+++ FaceRecMVC/Controllers/TryController.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using FaceRecMVC.Models;
 using Microsoft.ProjectOxford.Face;
 
 namespace FaceRecMVC.Controllers
@@ -39,22 +43,89 @@ namespace FaceRecMVC.Controllers
             ViewBag.PersonGroups = listModel;
         }
 
+        private Color[] colors = new Color[] {
+            Color.Chocolate, Color.Aqua, Color.Beige,
+            Color.Blue, Color.Chartreuse, Color.Coral,
+            Color.Crimson, Color.Cyan, Color.DarkKhaki,
+            Color.Green
+        };
+
         [HttpPost]
         public async Task<ActionResult> Index(string PersonGroupID)
         {
             if (this.Request.Files.Count > 0)
             {
-                var faces = await faceServiceClient.DetectAsync(this.Request.Files[0].InputStream, true, true);
+                var faceDetectImageStream = new MemoryStream();
+                var ImageResultStream = new MemoryStream();
+
+                await this.Request.Files[0].InputStream.CopyToAsync(faceDetectImageStream);
+                faceDetectImageStream.Position = 0;
+
+                await faceDetectImageStream.CopyToAsync(ImageResultStream);
+                ImageResultStream.Position = 0;
+                faceDetectImageStream.Position = 0;
+
+                var faces = await faceServiceClient.DetectAsync(faceDetectImageStream, true, true);
                 var identities = await faceServiceClient.IdentifyAsync(PersonGroupID, faces.Select(x => x.FaceId).ToArray());
 
-                ViewBag.Identities = identities;
+                IList<FaceDetectedViewModel> facesDetected = new List<FaceDetectedViewModel>();
+
+                int colorIndex = 0;
+                foreach (var face in faces)
+                {
+                    facesDetected.Add(new FaceDetectedViewModel()
+                    {
+                        FaceID = face.FaceId,
+                        RectColor = colors[colorIndex++]
+                    });
+                }
+
+                Image image = Image.FromStream(ImageResultStream);
+                using (Graphics g = Graphics.FromImage(image))
+                {
+                    foreach (var identity in identities)
+                    {
+                        var faceDetected = facesDetected.First(x => x.FaceID == identity.FaceId);
+                        var face = faces.First(x => x.FaceId == identity.FaceId);
+
+                        var pen = new Pen(faceDetected.RectColor, 5);
+                        g.DrawRectangle(pen, new Rectangle(face.FaceRectangle.Left, face.FaceRectangle.Top, face.FaceRectangle.Width, face.FaceRectangle.Height));
+
+                        foreach (var candidate in identity.Candidates)
+                        {
+                            var person = await faceServiceClient.GetPersonAsync(PersonGroupID, candidate.PersonId);
+
+                            faceDetected.PersonDetected.Add(new PersonDetectedViewModel()
+                            {
+                                PersonID = candidate.PersonId,
+                                PersonName = person.Name,
+                                UserData = person.UserData,
+                                Coincidence = candidate.Confidence
+                            });
+                        }
+                    }
+                }
+
                 await SetPersonGroups();
 
-                foreach (var identity in identities)
-                {
-                }
+                ViewBag.ImageResult = DataUriContent(image, this.Request.Files[0].FileName);
+                return View(facesDetected);
             }
-            return View();
+
+            return RedirectToAction("Index");
+        }
+
+        public static string DataUriContent(Image image, string fileName)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                
+                return $"data:image/" +
+                        Path.GetExtension(fileName).Replace(".", "")
+                        + ";base64,"
+                        + Convert.ToBase64String(ms.ToArray());
+            }
         }
     }
 }
---
+++ FaceRecMVC/Models/PersonDetectedViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace FaceRecMVC.Models
+{
+    public class PersonDetectedViewModel
+    {
+        public Guid PersonID { get; set; }
+        public string PersonName { get; set; }
+        public string UserData { get; set; }
+        public double Coincidence { get; set; }
+    }
+
+    public class FaceDetectedViewModel
+    {
+        public Color RectColor { get; set; }
+        public Guid FaceID { get; set; }
+
+        public IList<PersonDetectedViewModel> PersonDetected { get; set; }
+
+        public FaceDetectedViewModel()
+        {
+            this.PersonDetected = new List<PersonDetectedViewModel>();
+        }
+    }
+}
